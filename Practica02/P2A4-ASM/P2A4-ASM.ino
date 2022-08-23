c13517006b45bf71ee54f30cc8a97769f1dc894d
san <47926338+chimchars@users.noreply.github.com>
2022-08-22 23:21:58 -0500

    Add files via upload
    file con comentarios

--- Practica02/P2A4-ASM/P2A4-ASM.ino
+++ Practica02/P2A4-ASM/P2A4-ASM.ino
@@ -6,24 +6,24 @@ void loop()
 {
 asm (
 "inicio: \n\t"
-"sbi 0x05,0x07 \n\t"
-"call tiempo \n\t"
-"cbi 0x05,0x07 \n\t"
-"call tiempo \n\t"
-"jmp main \n\t"
-"tiempo: \n\t"
-"LDI r22, 45 \n\t"
-"LOOP_3: \n\t"
-"LDI r21, 255 \n\t"
-"LOOP_2: \n\t"
-"LDI r20, 255 \n\t"
-"LOOP_1: \n\t"
-"DEC r20 \n\t"
+"sbi 0x05,0x07 \n\t" // empieza el escrito en el eeprom
+"call tiempo \n\t" // llama a la variable de tiempo
+"cbi 0x05,0x07 \n\t" // limpia bit en los registros de i/o 0x05 0x07
+"call tiempo \n\t" // llama tiempo
+"jmp main \n\t" // resetea el handler
+"tiempo: \n\t" //
+"LDI r22, 45 \n\t" //empieza el programa principal en el registro 22 ← 45
+"LOOP_3: \n\t" 
+"LDI r21, 255 \n\t" // empieza el programa principal en el registro 21 ← 255
+"LOOP_2: \n\t" //
+"LDI r20, 255 \n\t" //empieza el programa principal en el registro 20 ← 255 
+"LOOP_1: \n\t" //
+"DEC r20 \n\t" //decrementa registro 20
 "BRNE LOOP_1 \n\t"
-"DEC r21 \n\t"
+"DEC r21 \n\t" //decrementa registro 21
 "BRNE LOOP_2 \n\t"
-"DEC r22 \n\t"
+"DEC r22 \n\t" //decrementa registro 22
 "BRNE LOOP_3 \n\t"
-"ret \n\t"
+"ret \n\t" //termina el código 
 );
 }
